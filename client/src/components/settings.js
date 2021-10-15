78f7c0277fe30e7912b2dd6d39486bf6bc8202bf
Ryan Passer <rpasser93@gmail.com>
2021-10-15 11:12:16 -0400

    Set up state and event listeners for settings dispatches

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTeamById } from "../actions/actions"
 
 const Settings = () => {
+  const [teamNameEdit, setTeamNameEdit] = useState("");
+  const [minPlayers, setminPlayers] = useState(0);  
+  const [minSexNumber, setMinSexNumber] = useState(0);
 
   const paramId = window.location.pathname.substr(window.location.pathname.length - 24);
 
@@ -13,6 +16,44 @@ const Settings = () => {
   useEffect(() => {
     dispatch(fetchTeamById(paramId));
   }, [dispatch, paramId]);
+
+  const handleTeamNameEditChange = (e) => {
+    setTeamNameEdit(e.target.value);
+  }
+
+  const saveTeamNameEdit = () => {
+    //DISPATCH
+  }
+
+  const handleMinPlayerChange = (e) => {
+    setminPlayers(e.target.value)
+  }
+
+  const saveMinPlayers = () => {
+    if (isNaN(minPlayers)) {
+      return alert('Please enter a valid number.');
+    }
+    //DISPATCH
+  }
+
+  const handleMinSexNumChange = (e) => {
+    setMinSexNumber(e.target.value)
+  }
+
+  const saveMinSexNumber = () => {
+    if (isNaN(minSexNumber)) {
+      return alert('Please enter a valid number.');
+    }
+    //DISPATCH
+  }
+
+  const saveMinSpecificSex = () => {
+    //DISPATCH
+  }
+
+  const restoreDefaultSettings = () => {
+    //DISPATCH////////
+  }
 
   if (team) {
     return (
@@ -33,8 +74,8 @@ const Settings = () => {
                   <h5><strong><em>General</em></strong></h5>
                   <p className="text-start">Edit team name:</p>
                   <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder={`${team.teamName}`} />
-                    <button className="btn btn-outline-success" type="button">Change</button>
+                    <input type="text" className="form-control" defaultValue={`${team.teamName}`} onChange={(e)=>{handleTeamNameEditChange(e)}}/>
+                    <button className="btn btn-outline-success" type="button" onClick={()=>{saveTeamNameEdit()}}>Change</button>
                   </div>
                   </div>
                 </div>
@@ -52,8 +93,8 @@ const Settings = () => {
                   </div>
                   <div className="col">
                     <div className="input-group mb-3">
-                      <input type="text" className="form-control" placeholder={`${team.settings[0].minPlayers}`} />
-                      <button className="btn btn-outline-primary" type="button">Save</button>
+                      <input type="text" className="form-control" defaultValue={`${team.settings[0].minPlayers}`} onChange={(e)=>{handleMinPlayerChange(e)}} />
+                      <button className="btn btn-outline-primary" type="button" onClick={()=>saveMinPlayers()}>Save</button>
                     </div>
                   </div>
                 </div>
@@ -67,26 +108,26 @@ const Settings = () => {
                   <em>at least</em>
                   </div>
                   <div className="col-2 text-start">
-                   <input className="form-control form-control-sm" type="text" defaultValue={`${team.settings[0].sexMin.min}`}/>
+                   <input className="form-control form-control-sm" type="text" defaultValue={`${team.settings[0].sexMin.min}`} onChange={(e)=>{handleMinSexNumChange(e)}}/>
                   </div>
                   <div className="col-2">
                     <p><strong><em>{team.settings[0].sexMin.sex}s</em></strong></p>
                   </div>
                   <div className="col-2">
                     <div className="form-check form-switch">
-                      <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={()=>{console.log('click!')}}/>
+                      <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={()=>{saveMinSpecificSex()}}/>
                       <label className="form-check-label" for="flexSwitchCheckDefault"></label>
                     </div>
                   </div>
                   <div className="col-3 text-end">
-                    <button type="button" class="btn btn-outline-primary">Save</button>
+                    <button type="button" class="btn btn-outline-primary" onClick={()=>{saveMinSexNumber()}}>Save</button>
                   </div>
                 </div>
                
               <hr/>
               <div className="row">
                 <div className="col">
-                  <button type="button" className="btn btn-secondary defaults-btn">Restore Defaults</button>                
+                  <button type="button" className="btn btn-secondary defaults-btn" onClick={()=>{restoreDefaultSettings()}}>Restore Defaults</button>                
                 </div>
               </div>
               <br/>
